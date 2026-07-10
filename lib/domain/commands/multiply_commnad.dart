80a610663c3a35593af6dc2ff3d438a5740938d1
Jamf05 <jorgemogotocoro05@outlook.es>
2026-07-09 22:24:55 -0500

    Updated multiply_command.dart with a concrete implementation for multiplication.

--- lib/domain/commands/multiply_commnad.dart
+++ lib/domain/commands/multiply_commnad.dart
@@ -1,10 +1,17 @@
 import 'package:flutter_command_pattern/core/command.dart';
 
 class MultiplyCommand extends Command {
-  MultiplyCommand(super.receiver);
+  MultiplyCommand(super.receiver, this._value);
+
+  final int _value;
 
   @override
   void execute() {
-    // TODO: implement execute
+    receiver.multiply(_value);
+  }
+  
+  @override
+  void undo() {
+    receiver.divide(_value);
   }
 }
