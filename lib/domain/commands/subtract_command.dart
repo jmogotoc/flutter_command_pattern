529e54c8d3a5a965058f64058d7d44d4f6b3b07e
Jamf05 <jorgemogotocoro05@outlook.es>
2026-07-09 22:24:10 -0500

    Updated subtract_command.dart with a concrete implementation for subtraction.

--- lib/domain/commands/subtract_command.dart
+++ lib/domain/commands/subtract_command.dart
@@ -1,10 +1,18 @@
 import 'package:flutter_command_pattern/core/command.dart';
 
 class SubtractCommand extends Command {
-  SubtractCommand(super.receiver);
+  SubtractCommand(super.receiver, this._value);
+
+  final int _value;
+
 
   @override
   void execute() {
-    // TODO: implement execute
+    receiver.subtract(_value);
+  }
+  
+  @override
+  void undo() {
+    receiver.add(_value);
   }
 }
