0deaf40db527215d2f166748049974a3fc38af6d
Jamf05 <jorgemogotocoro05@outlook.es>
2026-07-09 22:23:20 -0500

    Updated addition_command.dart with a concrete implementation for addition.

--- lib/domain/commands/addition_command.dart
+++ lib/domain/commands/addition_command.dart
@@ -1,10 +1,17 @@
 import 'package:flutter_command_pattern/core/command.dart';
 
 class AdditionCommand extends Command {
-  AdditionCommand(super.receiver);
+  AdditionCommand(super.receiver, this._value);
+
+  final int _value;
 
   @override
   void execute() {
-    // TODO: implement execute
+    receiver.add(_value);
+  }
+  
+  @override
+  void undo() {
+    receiver.subtract(_value);
   }
 }
