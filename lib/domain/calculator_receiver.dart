997363d079164b51e304a6eaf7de5c03b9e1a9a0
Jamf05 <jorgemogotocoro05@outlook.es>
2026-07-09 22:22:02 -0500

    Updated calculator_receiver.dart with a concrete implementation.

--- lib/domain/calculator_receiver.dart
+++ lib/domain/calculator_receiver.dart
@@ -1,3 +1,17 @@
 import 'package:flutter_command_pattern/core/receiver.dart';
 
-class CalculatorReceiver extends Receiver {}
+class CalculatorReceiver extends Receiver {
+  int _result = 0;
+
+  @override
+  void add(int value) => _result += value;
+  @override
+  void subtract(int value) => _result -= value;
+  @override
+  void multiply(int value) => _result *= value;
+  @override
+  void divide(int value) => _result ~/= value;
+
+  @override
+  int get result => _result;
+}
