ec0847a67d91e9c43dc034daf7608f33fa23936f
Jamf05 <jorgemogotocoro05@outlook.es>
2026-07-09 22:25:49 -0500

    Updated divide_command.dart with a concrete implementation for division.

--- lib/domain/commands/divide_command.dart
+++ lib/domain/commands/divide_command.dart
@@ -1,10 +1,17 @@
 import 'package:flutter_command_pattern/core/command.dart';
 
 class DivideCommand extends Command {
-  DivideCommand(super.receiver);
+  DivideCommand(super.receiver, this._value);
+
+  final int _value;
 
   @override
   void execute() {
-    // TODO: implement execute
+    receiver.divide(_value);
+  }
+  
+  @override
+  void undo() {
+    receiver.multiply(_value);
   }
 }
