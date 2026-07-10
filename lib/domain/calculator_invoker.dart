899609ef31d457d7d5b7e7aef853db6d7bbb8c31
Jamf05 <jorgemogotocoro05@outlook.es>
2026-07-09 22:21:44 -0500

    Updated calculator_invoker.dart with a concrete implementation.

--- lib/domain/calculator_invoker.dart
+++ lib/domain/calculator_invoker.dart
@@ -1,8 +1,22 @@
+import 'package:flutter_command_pattern/core/command.dart';
 import 'package:flutter_command_pattern/core/invoker.dart';
 
 class CalculatorInvoker extends Invoker {
+  CalculatorInvoker(super.receiver);
+
+  final List<Command> _history = [];
+
   @override
-  void execute() {
-    // TODO: implement execute
+  void execute(Command command) {
+    command.execute();
+    _history.add(command);
+  }
+
+  @override
+  void undo() {
+    if (_history.isNotEmpty) {
+      final lastCommand = _history.removeLast();
+      lastCommand.undo();
+    }
   }
 }
