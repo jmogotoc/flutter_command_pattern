dde574ef2f0089d9d647ce5cdaf816a2f67d865f
Jamf05 <jorgemogotocoro05@outlook.es>
2026-07-14 17:05:01 -0500

    Added option_button_enum.dart enum with the definition of the operations supported by the calculator

---
+++ lib/core/types/option_button_enum.dart
@@ -0,0 +1,10 @@
+enum OptionButtonEnum {
+  addition('+'),
+  subtraction('-'),
+  multiplication('x'),
+  division('÷'),
+  equals('=');
+
+  final String label;
+  const OptionButtonEnum(this.label);
+}
