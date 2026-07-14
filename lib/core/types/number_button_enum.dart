f507da2ff6660a7ecc16554cadd3f82fdef8b5a7
Jamf05 <jorgemogotocoro05@outlook.es>
2026-07-14 17:04:25 -0500

    Added number_button_enum.dart enum with the definition of numbers supported by the calculator

---
+++ lib/core/types/number_button_enum.dart
@@ -0,0 +1,15 @@
+enum NumberButtonEnum {
+  one(1),
+  two(2),
+  three(3),
+  four(4),
+  five(5),
+  six(6),
+  seven(7),
+  eight(8),
+  nine(9),
+  zero(0);
+
+  final int value;
+  const NumberButtonEnum(this.value);
+}
