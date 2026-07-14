74c36e99f046ae014ebe033cd5eb3a194c4f4758
Jamf05 <jorgemogotocoro05@outlook.es>
2026-07-14 16:46:32 -0500

    Updated calculator_command_button.dart and calculator_number_button.dart with the layout of the calculator buttons

---
+++ lib/ui/widgets/calculator_command_button.dart
@@ -0,0 +1,45 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class CalculatorCommandButton extends StatelessWidget {
+  final double width;
+  final double height;
+  final String value;
+  const CalculatorCommandButton({
+    super.key,
+    required this.value,
+    required this.width,
+    required this.height,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final double fontSize = min(width, height) * 0.5;
+    return SizedBox(
+      width: width,
+      height: height,
+      child: Container(
+        alignment: Alignment.center,
+        margin: EdgeInsets.all(2.0),
+        child: Material(
+          color: Colors.grey[300],
+          borderRadius: BorderRadius.circular(18),
+          child: InkWell(
+            borderRadius: BorderRadius.circular(18),
+            onTap: () {},
+            child: Center(
+              child: Text(
+                value,
+                style: TextStyle(
+                  fontSize: fontSize,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
