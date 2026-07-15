c81ed72f01427f98939200dd20319f385c1ff3be
Jamf05 <jorgemogotocoro05@outlook.es>
2026-07-15 18:25:35 -0500

    Simplified calculator_widget.dart by removing logic outside its responsibility

--- lib/ui/widgets/calculator_widget.dart
+++ lib/ui/widgets/calculator_widget.dart
@@ -4,7 +4,7 @@ import 'package:flutter_command_pattern/core/types/option_button_enum.dart';
 import 'package:flutter_command_pattern/ui/widgets/calculator_command_button.dart';
 import 'package:flutter_command_pattern/ui/widgets/calculator_number_button.dart';
 
-class CalculatorWidget extends StatefulWidget {
+class CalculatorWidget extends StatelessWidget {
   final void Function(NumberButtonEnum) onNumberTap;
   final void Function(OptionButtonEnum) onOptionTap;
 
@@ -15,35 +15,26 @@ class CalculatorWidget extends StatefulWidget {
   });
 
   @override
-  State<CalculatorWidget> createState() => _CalculatorWidgetState();
-}
-
-class _CalculatorWidgetState extends State<CalculatorWidget> {
-  final List<OptionButtonEnum> options = [
-    OptionButtonEnum.addition,
-    OptionButtonEnum.subtraction,
-    OptionButtonEnum.multiplication,
-    OptionButtonEnum.division,
-    OptionButtonEnum.equals,
-  ];
-  final List<NumberButtonEnum> numbers = [
-    NumberButtonEnum.one,
-    NumberButtonEnum.two,
-    NumberButtonEnum.three,
-    NumberButtonEnum.four,
-    NumberButtonEnum.five,
-    NumberButtonEnum.six,
-    NumberButtonEnum.seven,
-    NumberButtonEnum.eight,
-    NumberButtonEnum.nine,
-    NumberButtonEnum.zero,
-  ];
-
-  bool? isLastNumber;
-  bool? isLastOption;
-  
-  @override
   Widget build(BuildContext context) {
+    final List<OptionButtonEnum> options = [
+      OptionButtonEnum.addition,
+      OptionButtonEnum.subtraction,
+      OptionButtonEnum.multiplication,
+      OptionButtonEnum.division,
+      OptionButtonEnum.equals,
+    ];
+    final List<NumberButtonEnum> numbers = [
+      NumberButtonEnum.one,
+      NumberButtonEnum.two,
+      NumberButtonEnum.three,
+      NumberButtonEnum.four,
+      NumberButtonEnum.five,
+      NumberButtonEnum.six,
+      NumberButtonEnum.seven,
+      NumberButtonEnum.eight,
+      NumberButtonEnum.nine,
+      NumberButtonEnum.zero,
+    ];
     return LayoutBuilder(
       builder: (BuildContext context, BoxConstraints constraints) {
         return Row(
@@ -61,13 +52,8 @@ class _CalculatorWidgetState extends State<CalculatorWidget> {
                     number: numbers[index],
                     width: constraints.maxWidth * (0.8 / 3),
                     height: constraints.maxHeight / 4,
-                    onTap: (NumberButtonEnum number) {
-                      if(isLastOption == null || isLastOption == true) {
-                        widget.onNumberTap(number);
-                        isLastNumber = true;
-                        isLastOption = false;
-                      }
-                    },
+                    onTap: (NumberButtonEnum number) =>
+                        onNumberTap.call(number),
                   ),
                 ),
               ),
@@ -83,13 +69,8 @@ class _CalculatorWidgetState extends State<CalculatorWidget> {
                     option: options[index],
                     width: constraints.maxWidth * 0.2,
                     height: constraints.maxHeight / options.length,
-                    onTap: (OptionButtonEnum option) {
-                      if(isLastNumber == true) {
-                        widget.onOptionTap(option);
-                        isLastNumber = false;
-                        isLastOption = true;
-                      }
-                    },
+                    onTap: (OptionButtonEnum option) =>
+                        onOptionTap.call(option),
                   ),
                 ),
               ),
